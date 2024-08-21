09f5f519f481c973b2bbc7d936ba6a5685ebb4e1
Pablo <pablo.mrtnz23@gmail.com>
2024-08-21 02:12:03 -0300

    1er commit: html, css y js

---
+++ js/login_script.js
@@ -0,0 +1,16 @@
+function validarFormulario(event) {
+    const form = document.getElementById('registerForm');
+    const inputs = form.querySelectorAll('input[required]');
+
+    // Verifica si algún campo requerido está vacío
+    for (let input of inputs) {
+        if (input.value.trim() === '') {
+            event.preventDefault(); // Evita que el formulario se envíe
+            alert('-> Hay campos vacíos en el formulario! \n-> Por favor completar...');
+            return; // Sale de la función
+        }
+    }
+
+    // Si todos los campos requeridos están llenos, se muestra la alerta de éxito
+    alert("-> USUARIO REGISTRADO! \n-> Click en 'Aceptar' para dirigirse a la página principal...");
+}
---
+++ js/usuario_script.js
@@ -0,0 +1,17 @@
+function validarFormulario(event) {
+    const form = document.getElementById('registerForm');
+    const inputs = form.querySelectorAll('input[required]');
+
+    // Verifica si algún campo requerido está vacío
+    for (let input of inputs) {
+        if (input.value.trim() === '') {
+            event.preventDefault(); // Evita que el formulario se envíe
+            alert('-> Hay campos vacíos en el formulario! \n-> Por favor completar...');
+            return; // Sale de la función
+        }
+    }
+
+    // Si todos los campos requeridos están llenos, se muestra la alerta de éxito
+    alert("-> USUARIO REGISTRADO EXITOSAMENTE! \n-> Click en 'Aceptar' para dirigirse el registro de vehículos...");
+}
+
